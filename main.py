a4f1a525fcd76f791023fd41a0234713d14b08fc
CoronelGrasoso <109289702+CoronelGrasoso@users.noreply.github.com>
2022-11-30 20:10:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Import all modules
 import os
 import pygame
 import platform
 
+#// Some variables
 opsys = platform.system()
 
 sounds_folder = "sounds"
@@ -12,12 +14,13 @@ music = pygame.mixer.music
 playing = ""
 idx = -1
 paused = False
+#//
 
-volume_step = 1
-
+# Clamp function (very useful)
 def clamp(num, min_value, max_value):
    return max(min(num, max_value), min_value)
 
+#// Functions relationated with sounds
 def get_sounds():
 	return os.listdir(sounds_folder)
 
@@ -28,7 +31,9 @@ def set_playing(name):
 	playing = name
 	music.load(f"{sounds_folder}/{name}")
 	music.play()
+#//
 
+# Clear function, with the platform module i can make this project compatible with Windows and Linux
 def clear():
 	if opsys == "Windows":
 		os.system("cls")
@@ -42,16 +47,19 @@ if __name__ == "__main__":
 		dec = int(input())
 
 		if dec == 1:
+			# Code for going to the next sound
 			idx = clamp(idx + 1, 0, len(get_sounds()) - 1)
 			set_playing(get_name_idx(idx))
 			clear()
 			print(get_name_idx(idx))
 		elif dec == 2:
+			# Code for going to the previous sound
 			idx = clamp(idx - 1, 0, len(get_sounds()) - 1)
 			set_playing(get_name_idx(idx))
 			clear()
 			print(get_name_idx(idx))
 		elif dec == 3:
+			# Code for the pause/unpause command
 			paused = not paused
 
 			if paused:
